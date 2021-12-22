2ad5d80a06695f7cd3cd5130ac66319645ec811e
toblu96 <tobias.blum@besonet.ch>
2021-12-22 22:05:56 +0100

    New Volumes module (only query with filters)

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,7 +1,8 @@
 import { createApplication } from 'graphql-modules';
 import { UserModule } from './modules/container';
+import { VolumesModule } from './modules/volumes';
 
 export const application = createApplication({
-    modules: [UserModule],
+    modules: [UserModule, VolumesModule],
 });
 
